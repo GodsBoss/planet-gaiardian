77e360535819bacf62db26e630c2372baf4dcfd9
Timo Reitz <godsboss@gmx.de>
2015-12-12 09:45:58 +0100

    Instead of creating ordinary objects, introduce constructors to create the states.

--- src/js/game.js
+++ src/js/game.js
@@ -8,77 +8,62 @@ var game = (function(_, Phaser) {
 
   myGame.create = function(domId) {
     var game = new Phaser.Game(640, 400, Phaser.AUTO, 'game', null, !TRANSPARENT, !ANTIALIASING);
-    game.state.add('boot', createBoot(), AUTOSTART)
-    game.state.add('preload', createPreload());
-    game.state.add('selectLevel', createSelectLevel());
-    game.state.add('play', createPlay());
-    game.state.add('showLevelResult', createShowLevelResult());
+    game.state.add('boot', new Boot(), AUTOSTART)
+    game.state.add('preload', new Preload());
+    game.state.add('selectLevel', new SelectLevel());
+    game.state.add('play', new Play());
+    game.state.add('showLevelResult', new ShowLevelResult());
   };
 
-  return myGame;
+  function Boot() {}
 
-function createBoot()
-{
-  return {
-    preload: function() {
-      this.load.image('preload-screen', 'gfx/preload-screen.png');
-      this.load.image('preload-bar', 'gfx/preload-bar.png');
-    },
-    create: function() {
-      this.state.start('preload');
-    }
-  }
-}
-
-function createPreload()
-{
-  return {
-    preload: function() {
-      var preloaderScreen = this.add.sprite(this.world.centerX, this.world.centerY, 'preload-screen');
-      preloaderScreen.anchor.setTo(0.5, 0.5);
-      preloaderScreen.scale.setTo(2, 2);
-      var preloaderBar = this.add.sprite(this.world.centerX, this.world.centerY, 'preload-bar');
-      preloaderBar.anchor.setTo(0.5, 0.5);
-      this.load.setPreloadSprite(preloaderBar);
-      this.load.image('select-level-screen', 'gfx/select-level-screen.png');
-    },
-    create: function() {
-      this.state.start('selectLevel');
-    }
-  }
-}
-
-function createSelectLevel()
-{
-  return {
-    create: function() {
-      var screen = this.add.sprite(this.world.centerX, this.world.centerY, 'select-level-screen');
-      screen.anchor.setTo(0.5, 0.5);
-      screen.scale.setTo(2, 2);
-    },
-    update: function() {
-    }
+  Boot.prototype.preload = function() {
+    this.load.image('preload-screen', 'gfx/preload-screen.png');
+    this.load.image('preload-bar', 'gfx/preload-bar.png');
   };
-}
 
-function createPlay()
-{
-  return {
-    create: function() {
-    },
-    update: function() {
-    }
+  Boot.prototype.create = function() {
+    this.state.start('preload');
   }
-}
 
-function createShowLevelResult()
-{
-  return {
-    create: function() {
-    },
-    update: function() {
-    }
-  }
-}
+  function Preload(){}
+
+  Preload.prototype.preload = function() {
+    var preloaderScreen = this.add.sprite(this.world.centerX, this.world.centerY, 'preload-screen');
+    preloaderScreen.anchor.setTo(0.5, 0.5);
+    preloaderScreen.scale.setTo(2, 2);
+    var preloaderBar = this.add.sprite(this.world.centerX, this.world.centerY, 'preload-bar');
+    preloaderBar.anchor.setTo(0.5, 0.5);
+    this.load.setPreloadSprite(preloaderBar);
+    this.load.image('select-level-screen', 'gfx/select-level-screen.png');
+  };
+
+  Preload.prototype.create = function() {
+    this.state.start('selectLevel');
+  };
+
+  function SelectLevel() {}
+
+  SelectLevel.prototype.create = function() {
+    var screen = this.add.sprite(this.world.centerX, this.world.centerY, 'select-level-screen');
+    screen.anchor.setTo(0.5, 0.5);
+    screen.scale.setTo(2, 2);
+  };
+
+  SelectLevel.prototype.update = function() {}
+
+  function Play() {}
+
+  Play.prototype.update = function() {}
+
+  Play.prototype.update = function() {}
+
+  function ShowLevelResult() {}
+
+  ShowLevelResult.prototype.update = function() {}
+
+  ShowLevelResult.prototype.update = function() {}
+
+return myGame;
 
 })(_, Phaser);
