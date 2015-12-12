a694d92cd9654dfed36c3351ad26293f1c8508ea
Timo Reitz <godsboss@gmx.de>
2015-12-12 04:49:41 +0100

    Clarify Phaser.Game construction parameters.

--- src/js/game.js
+++ src/js/game.js
@@ -1,9 +1,12 @@
 var game = (function(_, Phaser) {
 
+  var TRANSPARENT = true;
+  var ANTIALIASING = true;
+
   var myGame = {};
 
   myGame.create = function(domId) {
-    var game = new Phaser.Game(640, 400, Phaser.AUTO, 'game', null, false, true);
+    var game = new Phaser.Game(640, 400, Phaser.AUTO, 'game', null, !TRANSPARENT, !ANTIALIASING);
   };
 
   return myGame;
