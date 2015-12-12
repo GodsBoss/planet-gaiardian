d286a5dc9e37ce83d8e3dfb7d3603e00fa00bca6
Timo Reitz <godsboss@gmx.de>
2015-12-12 15:20:47 +0100

    Let GIMP script really remove helper layer without breaking creation of PNGs without helper layer.

--- src/scripts/xcf2png.sh
+++ src/scripts/xcf2png.sh
@@ -2,6 +2,6 @@
 
 gimp -i -b "(let* ((image (car (gimp-file-load RUN-NONINTERACTIVE \"./src/gfx/$1.xcf\" \"$1.xcf\")))
 	(helper-layer (car (gimp-image-get-layer-by-name image \"Helpers\"))))
-	(if image helper-layer (gimp-image-remove-layer image helper-layer))
+	(catch #t (gimp-image-remove-layer image helper-layer))
 	 (file-png-save RUN-NONINTERACTIVE image (car (gimp-image-merge-visible-layers image EXPAND-AS-NECESSARY)) \"./dist/gfx/$1.png\" \"$1.png\" 0 9 0 0 0 0 0)
 	(gimp-quit 0))"
